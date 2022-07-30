d2102f4e4ddee5d5aee93844149ba3795a156245
Ashad001 <93534298+Ashad001@users.noreply.github.com>
2022-07-30 15:07:51 +0500

    BinarySearch

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int BinarySearch(vector<int> arr, int target)
 {
     int start = 0;
-    int end = arr.size();
+    int end = arr.size() - 1;
     while(start < end)
     {
         int mid = (start + end) / 2;
